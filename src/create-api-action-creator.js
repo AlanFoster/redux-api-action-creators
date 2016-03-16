aa9d057cfc3c15b9922fea6ec312b037baecffb5
Alan Foster <alan@alanfoster.me>
2016-03-16 23:20:53 +0000

    Add pending dispatch

--- src/create-api-action-creator.js
+++ src/create-api-action-creator.js
@@ -1,11 +1,29 @@
+import _ from 'lodash';
 import validateConfiguration from './validate-configuration';
+
+const actionsFor = function (configuration) {
+  const { pending } = configuration;
+
+  return {
+    pending() {
+      return {
+        type: pending
+      }
+    }
+  };
+};
 
 export default  function (configuration, actionCreatorName) {
   validateConfiguration(configuration, function (error) {
     throw new Error(`Can not create ApiActionCreator '${actionCreatorName}' because '${error}'`);
   });
 
-  return function () {
+  return () => (dispatch) => {
+    const actions = actionsFor(configuration);
+
+    if (_.isString(configuration.pending)) {
+      dispatch(actions.pending())
+    }
 
   };
 };
--- src/tests/create-api-action-creator.spec.js
+++ src/tests/create-api-action-creator.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
-import createApiActionCreator from '../create-api-action-creator'
+import sinon from 'sinon';
 import _ from 'lodash';
+import createApiActionCreator from '../create-api-action-creator'
 
 describe('CreateApiActionCreator', function () {
   describe('when given a valid configuration', function () {
@@ -19,6 +20,55 @@ describe('CreateApiActionCreator', function () {
     it('creates the action creator function', function () {
       expect(this.actionCreator).to.be.a('Function');
     });
-  })
+  });
+
+  describe('pending dispatch', function () {
+    context('when the pending action is missing', function () {
+      beforeEach(function () {
+        const actionCreatorName = 'getData';
+        const configuration = {
+          route: '/data',
+          success: 'DATA_SUCCESS',
+          error: 'DATA_ERROR'
+        };
+
+        this.actionCreator = createApiActionCreator(configuration, actionCreatorName);
+
+        this.dispatchSpy = sinon.spy();
+        const actionArguments = undefined;
+        this.actionCreator(actionArguments)(this.dispatchSpy);
+      });
+
+      it('does not trigger a pending dispatch', function () {
+        expect(this.dispatchSpy).not.to.have.been.calledWith({
+          type: 'DATA_PENDING'
+        });
+      });
+    });
+
+    context('when the pending action is present', function () {
+      beforeEach(function () {
+        const actionCreatorName = 'getData';
+        const configuration = {
+          route: '/data',
+          success: 'DATA_SUCCESS',
+          pending: 'DATA_PENDING',
+          error: 'DATA_ERROR'
+        };
+
+        this.actionCreator = createApiActionCreator(configuration, actionCreatorName);
+
+        this.dispatchSpy = sinon.spy();
+        const actionArguments = undefined;
+        this.actionCreator(actionArguments)(this.dispatchSpy);
+      });
+
+      it('does trigger a pending dispatch', function () {
+        expect(this.dispatchSpy).to.have.been.calledWith({
+          type: 'DATA_PENDING'
+        });
+      });
+    });
+  });
 });
 
