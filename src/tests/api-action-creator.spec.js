2472eac322f589eccd0bbf3c997e79b1889bc74b
Alan Foster <alan@alanfoster.me>
2016-03-16 22:00:03 +0000

    Add babel + test configuration

---
+++ src/tests/api-action-creator.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import ApiActionCreator from '../'
+
+describe('ApiActionCreator', function () {
+  it('exists', function () {
+    expect(ApiActionCreator).to.eql({});
+  });
+});
