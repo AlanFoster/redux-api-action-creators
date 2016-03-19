68edae4f72da26eafb17807f32aad5f9584b0d00
Alan Foster <alan@alanfoster.me>
2016-03-19 01:18:29 +0000

    Expose JSON API creators via api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
 import createAPIActionCreator from './create-api-action-creator';
+import createJSONAPIActionCreator from './create-json-api-action-creator';
 
 export default {
   createAPIActionCreators(configuration) {
     const actionCreators = _.mapValues(configuration, createAPIActionCreator);
+
+    return actionCreators;
+  },
+
+  createJSONAPIActionCreators(configuration) {
+    const actionCreators = _.mapValues(configuration, createJSONAPIActionCreator);
 
     return actionCreators;
   }
--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -8,10 +8,13 @@ describe('External API', function () {
     this.createAPIActionCreatorMock.onCall(0).returns('firstResult');
     this.createAPIActionCreatorMock.onCall(1).returns('secondResult');
 
+    this.createJSONAPIActionCreatorMock = sinon.stub();
+    this.createJSONAPIActionCreatorMock.onCall(0).returns('firstJSONResult');
+    this.createJSONAPIActionCreatorMock.onCall(1).returns('secondJSONResult');
+
     return proxyquire('../index', {
       './create-api-action-creator': this.createAPIActionCreatorMock,
-      './create-json-api-action-creator': this.createAPIActionCreatorMock,
-
+      './create-json-api-action-creator': this.createJSONAPIActionCreatorMock
     }).default;
   };
 
@@ -50,6 +53,42 @@ describe('External API', function () {
         expect(this.actionCreators).to.eql({
           firstRoute: 'firstResult',
           secondRoute: 'secondResult'
+        });
+      });
+    })
+  });
+
+  describe('#createJSONAPIActionCreators', function () {
+    describe('when passed no configuration', function () {
+      beforeEach(function () {
+        this.actionCreators = api().createJSONAPIActionCreators({});
+      });
+
+      it('returns no dispatchers', function () {
+        expect(this.actionCreators).to.eql({});
+      });
+    });
+
+    describe('when passed configuration', function () {
+      beforeEach(function () {
+        this.actionCreators = api().createJSONAPIActionCreators({
+          firstRoute: { route: 'first' },
+          secondRoute: { route: 'second' }
+        });
+      });
+
+      it('calls the createJSONAPIActionCreatorMock with the first route', function () {
+        expect(this.createJSONAPIActionCreatorMock).to.have.been.calledWith({ route: 'first'}, 'firstRoute');
+      });
+
+      it('calls the createJSONAPIActionCreatorMock with the second route', function () {
+        expect(this.createJSONAPIActionCreatorMock).to.have.been.calledWith({ route: 'second'}, 'secondRoute');
+      });
+
+      it('returns the action creators', function () {
+        expect(this.actionCreators).to.eql({
+          firstRoute: 'firstJSONResult',
+          secondRoute: 'secondJSONResult'
         });
       });
     })
