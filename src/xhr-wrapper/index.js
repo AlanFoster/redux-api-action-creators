c5b451899e1b00d703edc801a554e9bd66b9e66f
Alan Foster <alan@alanfoster.me>
2016-03-20 00:26:25 +0000

    Add support for headers an post requests

--- src/xhr-wrapper/index.js
+++ src/xhr-wrapper/index.js
@@ -1,8 +1,9 @@
+import _ from 'lodash';
 import XMLHttpRequest from './raw-xhr';
 import isSuccess from './is-success';
 
 export default function (request, onSuccess) {
-  const { method = 'GET', url } = request;
+  const { method = 'GET', url, body, headers = {} } = request;
 
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
@@ -11,6 +12,11 @@ export default function (request, onSuccess) {
     }
   };
 
-  xhr.open(method, url);
-  xhr.send();
+  xhr.open(method, url, true);
+
+  _.each(headers, function (value, header) {
+    xhr.setRequestHeader(header, value);
+  });
+
+  xhr.send(body);
 }
