1dd6a182d6a489c7526da64d0ad7c025db2a73a6
Alan Foster <alan@alanfoster.me>
2016-03-20 16:47:58 +0000

    Pass action creator params

--- src/create-api-action-creator/request-for.js
+++ src/create-api-action-creator/request-for.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
 import urlFor from './url-for';
 
-export default function (configuration) {
-  const request = _.result(configuration, 'createRequest', { });
+export default function (configuration, ...args) {
+  configuration = _.defaults(configuration, {
+    createRequest: _.constant({})
+  });
 
+  const request = configuration.createRequest(...args);
   const url = urlFor(request, configuration.route);
   const method = _.result(configuration, 'method', 'GET');
 
--- src/create-api-action-creator/tests/request-for.spec.js
+++ src/create-api-action-creator/tests/request-for.spec.js
@@ -15,8 +15,18 @@ describe('RequestFor', function () {
   describe('when given a concrete request implementation', function () {
     context('and no request params are provided', function () {
       beforeEach(function () {
-        const createRequestResult = { body: 'Hello World', headers: { 'Custom-Header': true } };
-        this.result = requestFor({ route: '/data', method: 'POST', createRequest: () => createRequestResult });
+        const createRequest = function () {
+          return {
+            body: 'Hello World',
+            headers: { 'Custom-Header': true }
+          };
+        };
+
+        this.result = requestFor({
+          route: '/data',
+          method: 'POST',
+          createRequest
+        });
       });
 
       it('returns the route method, body, and headers', function () {
@@ -30,6 +40,40 @@ describe('RequestFor', function () {
         });
       });
     });
-  })
+
+    context('and request params are provided', function () {
+      beforeEach(function () {
+        const createRequest = function (blog) {
+          return {
+            headers: { 'Custom-Header': true },
+            params: {
+              id: blog.id
+            },
+            body: 'fake body'
+          };
+        };
+
+        this.result = requestFor({
+          route: '/blog/:id/vote',
+          method: 'POST',
+          createRequest
+        }, { id: 123 });
+      });
+
+      it('returns the route method, body, and headers', function () {
+        expect(this.result).to.eql({
+          headers: {
+            'Custom-Header': true
+          },
+          params: {
+            id: 123
+          },
+          body: 'fake body',
+          method: 'POST',
+          url: '/blog/123/vote'
+        });
+      });
+    });
+  });
 });
 
