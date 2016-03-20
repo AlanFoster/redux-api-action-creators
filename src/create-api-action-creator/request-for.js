07324beff0fd54fdb07f7d4911b226e2086cc647
Alan Foster <alan@alanfoster.me>
2016-03-20 16:26:20 +0000

    Correctly set method type

--- src/create-api-action-creator/request-for.js
+++ src/create-api-action-creator/request-for.js
@@ -3,8 +3,9 @@ import urlFor from './url-for';
 
 export default function (configuration) {
   const request = _.result(configuration, 'createRequest', { });
-  const method = _.result(request, 'method', 'GET');
+
   const url = urlFor(request, configuration.route);
+  const method = _.result(configuration, 'method', 'GET');
 
   return _.extend({}, request, { url, method });
 }
--- src/create-api-action-creator/tests/request-for.spec.js
+++ src/create-api-action-creator/tests/request-for.spec.js
@@ -4,7 +4,7 @@ import requestFor from '../request-for';
 describe('RequestFor', function () {
   describe('when given no request implementation', function () {
     beforeEach(function () {
-      this.result = requestFor({ route: '/data', createRequest: () => this.expectedResult });
+      this.result = requestFor({ route: '/data' });
     });
 
     it('returns an empty request object with only a GET URL', function () {
@@ -15,16 +15,19 @@ describe('RequestFor', function () {
   describe('when given a concrete request implementation', function () {
     context('and no request params are provided', function () {
       beforeEach(function () {
-        this.expectedResult = { body: 'Hello World', method: 'POST', route: '/data' };
-        this.result = requestFor({ route: '/data', createRequest: () => this.expectedResult });
+        const createRequestResult = { body: 'Hello World', headers: { 'Custom-Header': true } };
+        this.result = requestFor({ route: '/data', method: 'POST', createRequest: () => createRequestResult });
       });
 
-      it('returns the all existing values', function () {
-        expect(this.result).to.contain(this.expectedResult);
-      });
-
-      it('returns the url', function () {
-        expect(this.result.url).to.eql('/data');
+      it('returns the route method, body, and headers', function () {
+        expect(this.result).to.eql({
+          body: 'Hello World',
+          headers: {
+            'Custom-Header': true
+          },
+          method: 'POST',
+          url: '/data'
+        });
       });
     });
   })
